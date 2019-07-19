4bb8d76b9fa594f65cb94b858b4f7415e42a8ec6
Zach Price <37559274+zachchan105@users.noreply.github.com>
2019-07-19 11:28:54 -0700

    Notes

--- MiningPayoutCalculator.java
+++ MiningPayoutCalculator.java
@@ -1,13 +1,14 @@
 
 //Author: Zachary Price
 //Program: Mining payout calculator
+//Github: zachchan105
 
 import java.util.*;
-public class tester {
+public class MiningPayoutCalculator {
 
 	public static void main(String args[]) {
 
-
+		//Define and initizalize specific variables
 		int totalShares = 0;
 		int numDevices;
 		int shareCount;
@@ -15,31 +16,34 @@ public class tester {
 		double tmpPayout;
 		double tmpDouble;
 		double tmpDouble2;
-
 		String tmp;
 		String currency;
 
+		//Scanner for user input
 		Scanner scan = new Scanner(System.in);
 
 		System.out.println("<|*|> Welcome to the mining calculator! <|*|>");
 		System.out.println("");
 		System.out.println("Please enter the cryptocurrency you are mining:");
-		currency = scan.next();
+		currency = scan.next();//Store the cryptocurrency mined into a string
 		System.out.println("Crypto -- " + currency);
 		System.out.println("");
 		System.out.println("Please enter how many devices are mining on your pool:");
-		numDevices = scan.nextInt();
+		numDevices = scan.nextInt();//Store the amount of devices mining on a certain pool into an int
 		System.out.println("You have "+numDevices+" devices mining on your pool");
 		System.out.println("");
 		System.out.println("Please enter the amount of "+currency+ " mined:");
-		currencyMine = scan.nextDouble();
+		currencyMine = scan.nextDouble();//Store the amount of crypto mined into a double variable
 
 		int shares[] = new int[numDevices];
+		//Create an array the size of the number of devices on the pool
 
 		System.out.println("");
 
+		
+		//This for loop runs as many times as there is devices mining on the pool.
 		for(int i = 0; i < numDevices; i++) {
-
+			//Fills out the integer array with the number of shares for each device
 			System.out.println("Please enter the number of shares for device "+(i+1));
 			shareCount = scan.nextInt();
 			shares[i] = shareCount;
@@ -61,6 +65,7 @@ public class tester {
 		System.out.println("Done.");
 		System.out.println("");
 
+		//For loops naviagates through the array and adds the values in each position together
 		for(int i = 0; i <numDevices; i++) {
 			totalShares = totalShares + shares[i];
 		}
@@ -72,6 +77,7 @@ public class tester {
 
 		System.out.println("");
 		System.out.println("");
+		//Simple way to allow the user to view an amount of information for a desired time
 		System.out.println("Enter any letter or number to continue...");
 
 		tmp = scan.next();
@@ -80,9 +86,12 @@ public class tester {
 		System.out.println("");
 
 		System.out.println("------------------------------------------------");
+		
+		//This for loop runs as many times as there is devices mining on the pool.
 		for(int i = 0; i < numDevices; i++) {  //Using temporary doubles to prevent casting issues
 			tmpDouble = shares[i];
 			tmpDouble2 = totalShares;
+			//Equation for calculating the amount of crypto each device produced.
 			tmpPayout = currencyMine * (tmpDouble/tmpDouble2);
 			System.out.println("");
 			System.out.println("Payout for device " + i + ": " + tmpPayout);
